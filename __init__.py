d2d418ddec38722fe3e1854b992b330980da0450
TobiasKönemund <tobiaskoenemund@gmail.com>
2026-01-18 18:29:30 +0100

    removed previous methode

--- __init__.py
+++ __init__.py
@@ -1,15 +1,11 @@
-from .web_fetch_node import WebFetchNode
 from .web_fetch_server_node import WebFetchServerNode
 
 NODE_CLASS_MAPPINGS = {
-    "WebFetchNode": WebFetchNode,
     "WebFetchServerNode": WebFetchServerNode
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "WebFetchNode": "Web Fetch (Selenium)",
-    "WebFetchServerNode": "Web Fetch (Userscript / Bridge)"
+    "WebFetchServerNode": "Web Bridge (Tampermonkey)"
 }
-
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
